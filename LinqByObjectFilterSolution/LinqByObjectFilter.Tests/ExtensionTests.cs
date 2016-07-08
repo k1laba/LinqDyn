90824fb44c7034622a2f30b0a14bb742a84e1f32
Beka Kilaberia <Beka.Kilaberia@republic.ge>
2016-07-08 15:33:55 +0400

    renamed baseentity to product in tests project

--- LinqByObjectFilterSolution/LinqByObjectFilter.Tests/ExtensionTests.cs
+++ LinqByObjectFilterSolution/LinqByObjectFilter.Tests/ExtensionTests.cs
@@ -8,19 +8,19 @@ namespace LinqByObjectFilter.Tests
 {
     public class ExtensionsTests
     {
-        List<BaseEntity> _list = new List<BaseEntity>();
+        List<Product> _list = new List<Product>();
         public ExtensionsTests()
         {
-            _list.Add(new BaseEntity() { Id = 3, SortOrder = 3, Name = "123" });
-            _list.Add(new BaseEntity() { Id = 1, SortOrder = 1, Name = "abc" });
-            _list.Add(new BaseEntity() { Id = 8, SortOrder = 8, Name = "987" });
-            _list.Add(new BaseEntity() { Id = 4, SortOrder = 4, Name = "beqakilaba" });
-            _list.Add(new BaseEntity() { Id = 2, SortOrder = 20, Name = "456" });
-            _list.Add(new BaseEntity() { Id = 6, SortOrder = 6, Name = "test" });
-            _list.Add(new BaseEntity() { Id = 5, SortOrder = 5, Name = "kilaba" });
-            _list.Add(new BaseEntity() { Id = 10, SortOrder = 10, Name = "bek" });
-            _list.Add(new BaseEntity() { Id = 7, SortOrder = 7, Name = "beq" });
-            _list.Add(new BaseEntity() { Id = 9, SortOrder = 9, Name = "beqa" });
+            _list.Add(new Product() { Id = 3, SortOrder = 3, Name = "123" });
+            _list.Add(new Product() { Id = 1, SortOrder = 1, Name = "abc" });
+            _list.Add(new Product() { Id = 8, SortOrder = 8, Name = "987" });
+            _list.Add(new Product() { Id = 4, SortOrder = 4, Name = "beqakilaba" });
+            _list.Add(new Product() { Id = 2, SortOrder = 20, Name = "456" });
+            _list.Add(new Product() { Id = 6, SortOrder = 6, Name = "test" });
+            _list.Add(new Product() { Id = 5, SortOrder = 5, Name = "kilaba" });
+            _list.Add(new Product() { Id = 10, SortOrder = 10, Name = "bek" });
+            _list.Add(new Product() { Id = 7, SortOrder = 7, Name = "beq" });
+            _list.Add(new Product() { Id = 9, SortOrder = 9, Name = "beqa" });
         }
         [Fact]
         public void OrderBy_WhenCalls_ShouldOrderCorrectly()
@@ -46,7 +46,7 @@ namespace LinqByObjectFilter.Tests
         public void FilterBy_WhenCalls_ShouldOrderCorrectly()
         {
             //arrange
-            var filter = new BaseEntityFilter() { Id = 1, SortOrder = 5, Name = "beq", FromId = 4, ToId = 9 };
+            var filter = new ProductFilter() { Id = 1, SortOrder = 5, Name = "beq", FromId = 4, ToId = 9 };
             var expected = _list.Where(i => i.Id >= filter.FromId && i.Id <= filter.ToId)
                                 .Where(i => i.Name.Contains(filter.Name))
                                 .Where(i => i.SortOrder >= filter.SortOrder).ToList();
@@ -56,13 +56,13 @@ namespace LinqByObjectFilter.Tests
             Assert.True(expected.SequenceEqual(actual));
         }
     }
-    public class BaseEntity
+    public class Product
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public int SortOrder { get; set; }
     }
-    public class BaseEntityFilter
+    public class ProductFilter
     {
         [FilterInfo(FilterOperator.None)]
         public int Id { get; set; }
